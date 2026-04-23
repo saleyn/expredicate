0e7c6b0f73b6e54456bce67b9ba1998a95f0de75
Serge Aleynikov <saleyn@gmail.com>
2026-04-23 00:43:30 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Expredicate.MixProject do
   def project do
     [
       app:             :expredicate,
-      version:         "0.1.1",
+      version:         "0.1.2",
       elixir:          "~> 1.14",
       start_permanent: Mix.env() == :prod,
       elixirc_paths:   elixirc_paths(Mix.env()),
